d72fd90cfc2c9fa4e0029d45ba3fc61997a8ae1e
Philipp Reh <sefi@s-e-f-i.de>
2009-06-26 18:38:06 +0200

    Removed convert/fwd.hpp.

--- include/mizuiro/color/convert.hpp
+++ include/mizuiro/color/convert.hpp
@@ -1,7 +1,6 @@
 #ifndef MIZUIRO_COLOR_CONVERT_HPP_INCLUDED
 #define MIZUIRO_COLOR_CONVERT_HPP_INCLUDED
 
-#include <mizuiro/color/convert/fwd.hpp>
 #include <mizuiro/color/convert/identity.hpp>
 #include <mizuiro/color/convert/from_gray.hpp>
 
