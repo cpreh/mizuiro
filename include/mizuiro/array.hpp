1ecd0036b4280e9acec8b57a9c90efc4a02be998
Philipp Reh <sefi@s-e-f-i.de>
2013-01-19 16:50:37 +0100

    Use boost::array everywhere for now

--- include/mizuiro/array.hpp
+++ include/mizuiro/array.hpp
@@ -7,17 +7,9 @@
 #ifndef MIZUIRO_ARRAY_HPP_INCLUDED
 #define MIZUIRO_ARRAY_HPP_INCLUDED
 
-#include <mizuiro/config.hpp>
-#ifdef MIZUIRO_HAVE_FCPPT
-#include <fcppt/container/array.hpp>
-#include <fcppt/container/array_fusion.hpp>
-#else
 #include <mizuiro/detail/external_begin.hpp>
-#include <boost/fusion/adapted/boost_array.hpp>
 #include <boost/array.hpp>
-#include <mizuiro/detail/external_end.hpp>
-#endif
-#include <mizuiro/detail/external_begin.hpp>
+#include <boost/fusion/adapted/boost_array.hpp>
 #include <cstddef>
 #include <mizuiro/detail/external_end.hpp>
 
@@ -25,9 +17,10 @@
 namespace mizuiro
 {
 
-/// mizuiro's array type
 /**
- * Exposes a typedef ::type.
+\brief mizuiro's array type
+
+Exposes a typedef ::type.
 */
 template<
 	typename T,
@@ -35,17 +28,10 @@ template<
 >
 struct array
 {
-#ifdef MIZUIRO_HAVE_FCPPT
-	typedef fcppt::container::array<
-		T,
-		N
-	> type;
-#else
 	typedef boost::array<
 		T,
 		N
 	> type;
-#endif
 };
 
 }
