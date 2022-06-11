c75694054f26b4a8f4c583b5f32233cfaff2b44f
Carl Philipp Reh <carlphilippreh@gmail.com>
2022-06-11 19:18:39 +0200

    Updates for catch-3

--- test/dynamic.cpp
+++ test/dynamic.cpp
@@ -21,7 +21,7 @@
 #include <fcppt/catch/begin.hpp>
 #include <fcppt/catch/end.hpp>
 #include <fcppt/config/external_begin.hpp>
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
 #include <cstdint>
 #include <functional>
 #include <fcppt/config/external_end.hpp>
